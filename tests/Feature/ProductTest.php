209adc180b4e2ae56f9f19e9e80687e76673bdda
Wendy Sosa <wendy.p.sosa@gmail.com>
2019-09-24 15:48:25 -0500

    Show, Delete, Update Test Added

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 
 class ProductTest extends TestCase
 {
-     use RefreshDatabase;
+     //use RefreshDatabase;
 
      public function test_client_can_create_a_product()
     {
@@ -53,6 +53,75 @@ class ProductTest extends TestCase
                 'price' => '23.30'
             ]
         );
+    }
+
+     public function test_client_can_show_a_product() {
+      // Given
+        $id = '2';
+        
+        // When
+        $response = $this->json('GET', '/api/products/'.$id); 
+
+        // Then
+        // Assert it sends the correct HTTP Status
+        $response->assertStatus(200); 
+        
+        // Assert the response has the correct structure
+      $response->assertJsonStructure([
+           'id',
+            'name',
+            'price'
+        ]);
+
+        // Assert the product was RETURNED
+        // with the correct data
+        $response->assertJsonFragment([
+            'name' => 'Porta Taquitos updated',
+            'price' => '130.60'
+        ]);
+    }
+
+    public function test_client_can_update_a_product() {
+      // Given
+        $id = '2';
+        $productData = [
+            'name' => 'Porta Taquitos updated',
+            'price' => '130.60'
+        ];
+        
+        // When
+        $response = $this->json('PUT', '/api/products/'.$id, $productData); 
+
+        // Then
+        // Assert it sends the correct HTTP Status
+        $response->assertStatus(200); 
+        
+        // Assert the response has the correct structure
+      $response->assertJsonStructure([
+           'id',
+            'name',
+            'price'
+        ]);
+
+        // Assert the product was updated
+        // with the correct data
+        $response->assertJsonFragment([
+            'name' => 'Porta Taquitos updated',
+            'price' => '130.60'
+        ]);
+    }
+
+    public function test_client_can_delete_a_product() {
+      // Given
+        $id = '2';
+        
+        
+        // When
+        $response = $this->json('DELETE', '/api/products/'.$id); 
+
+        // Then
+        // Assert it sends the correct HTTP Status
+        $response->assertStatus(200); 
     }
 
 }
